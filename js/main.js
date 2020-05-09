d41ba1603af4d178e12299a0b7206655d027ec64
Claire Tregunna <c.tregunna1980@gmail.com>
2020-05-09 13:55:32 +0100

    added light box and alter some lightbox css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-$('header').sticky({
-    getWidthFrom: '.container',
-    responsiveWidth: true
-  });
+// $('header').sticky({
+//     getWidthFrom: '.container',
+//     responsiveWidth: true
+//   }); sticky header, bipping to the left so only use if can fix
+
+// Lightbox
+
+    lightbox.option({
+        showImageNumberLabel: false,
+        // albumLabel: '', same as above
+        wrapAround: true
+    })
